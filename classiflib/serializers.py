d6c27998a6778fe581b912c32e22b43402b3b14d
Michael V. DePalatis <mike@depalatis.net>
2017-09-28 11:47:43 -0400

    Rework HDF5 layout, break things up for testing

--- classiflib/serializers.py
+++ classiflib/serializers.py
@@ -1,8 +1,10 @@
 import os.path as osp
+from functools import partial
 
 import numpy as np
 import tables
 from sklearn import __version__ as sklearn_version
+from sklearn.externals import joblib
 
 from . import __version__, dtypes
 from .dtypes import with_id
@@ -24,9 +26,30 @@ class BaseSerializer(object):
         be a recarray with the dtype ``.dtypes.pairs``.
 
     """
-    def __init__(self, classifier, pairs):
+    def __init__(self, classifier, pairs, subject="undefined"):
+        # Indicates if this was generated from a legacy pickle file or not
+        self._from_legacy_format = False
+
         self.classifier = classifier
         self.pairs = self._validate_pairs(pairs)
+        self.subject = subject
+
+    @classmethod
+    def from_pickle(cls, pickle_file, pairs):
+        """Convert the legacy pickle format to modern formats.
+
+        Parameters
+        ----------
+        pickle_file : str or pathlib.Path
+            Pickle file to read
+        pairs : list or np.recarray
+            See :meth:`BaseSerializer.__init__` for details
+
+        """
+        classifier = joblib.load(pickle_file)
+        serializer = cls(classifier, pairs)
+        serializer._from_legacy_format = True
+        return serializer
 
     @staticmethod
     def _validate_pairs(pairs):
@@ -45,22 +68,53 @@ class BaseSerializer(object):
         return '.'.join([self.classifier.__module__,
                          self.classifier.__class__.__name__])
 
+    @property
+    def version(self):
+        """Generate a version string for the current class. Implementations
+        are required to have a ``_version`` attribute.
+
+        """
+        if not hasattr(self, '_version'):
+            raise NotImplementedError("Serializers must contain a _version attribute")
+        return "_".join([self.__class__.__name__, self._version])
+
     def serialize(self, *args, **kwargs):
         raise NotImplementedError
 
 
 class HDF5Serializer(BaseSerializer):
     """Utility class to serialize or deserialize a classifier using HDF5."""
-    __version = "1.0.0"
+    _version = "1.0.0"
 
-    def _create_hdf5(self, hfile):
-        # Create empty tables
+    def addstring(self, hfile, group, name, value):
+        """Base function for adding a string to a group; will be partialed to
+        only have to specify the group once.
+
+        """
+        hfile.create_array(group, name, obj=str.encode(value))
+
+    def add_attributes(self, hfile):
+        """Adds root node attributes:
+
+        * commit_hash - git commit hash when serialized from the classiflib git
+          repo
+
+        """
+        hfile.set_node_attr('/', 'commit_hash', git_revision())
+
+    def add_versions(self, hfile):
+        """Create version number node and add relevant versions."""
+        group = hfile.create_group('/', 'versions')
+        addstring = partial(self.addstring, hfile, group)
+        addstring('sklearn', sklearn_version)
+        addstring('classiflib', __version__)
+        addstring('classifier', CLASSIFIER_VERSION if not self._from_legacy_format else "1.0.0")
+        addstring('serialization', self.version)
+
+    def add_pairs(self, hfile):
+        """Create and populate pairs table."""
         hfile.create_table('/', 'pairs', with_id(dtypes.pairs),
                            title="Bipolar pairs", expectedrows=256)
-        # hfile.create_table('/', 'weights', Weight, title='Classifier weights',
-        #                    expectedrows=256)
-
-        # Populate pair info
         for i, pair in enumerate(self.pairs):
             row = hfile.root.pairs.row
             row['id'] = i
@@ -70,20 +124,26 @@ class HDF5Serializer(BaseSerializer):
             row['label2'] = pair[3]
             row.append()
 
-        # Add metadata
-        group = hfile.create_group('/', 'classifier_info')
-        addstring = lambda name, value: hfile.create_array(group, name, obj=str.encode(value))
+    def add_classifier(self, hfile):
+        """Create classifier group and add data."""
+        cgroup = hfile.create_group('/', 'classifier')
+        # hfile.create_table(cgroup, 'weights', Weight, title='Classifier weights',
+        #                    expectedrows=256)
+
+        info_group = hfile.create_group(cgroup, 'info')
+        addstring = partial(self.addstring, hfile, info_group)
         addstring('classname', self.classname)
-        addstring('sklearn_version', sklearn_version)
-        addstring('classifier_version', CLASSIFIER_VERSION)
-        addstring('classifier_utils_version', __version__)
-        addstring('serialization_version', self.__version)
-        addstring('commit_hash', git_revision())
+        addstring('subject', self.subject)
+
+    def _create_hdf5(self, filename):
+        with tables.open_file(filename, 'w') as hfile:
+            self.add_attributes(hfile)
+            self.add_versions(hfile)
+            self.add_pairs(hfile)
+            self.add_classifier(hfile)
 
     def serialize(self, filename, overwrite=True):
         """Serialize the data to the specified location."""
         if osp.exists(filename) and not overwrite:
             raise RuntimeError("{} already exists".format(filename))
-
-        with tables.open_file(filename, 'w') as hfile:
-            self._create_hdf5(hfile)
+        self._create_hdf5(filename)
--- classiflib/test/test_serializers.py
+++ classiflib/test/test_serializers.py
@@ -3,28 +3,41 @@ import tables
 import pytest
 
 from classiflib import dtypes
-from classiflib.serializers import HDF5Serializer
+from classiflib.serializers import BaseSerializer, HDF5Serializer
+
+
+@pytest.fixture
+def single_pair():
+    yield [(0, 1, 'A0', 'A1')]
 
 
 class DummyClassifier(object):
     pass
 
 
+class TestBaseSerializer:
+    def setup_method(self, method):
+        self.serializer = BaseSerializer(DummyClassifier(), [(0, 1, 'A0', 'A1')])
+
+    def test_version(self):
+        with pytest.raises(NotImplementedError):
+            _ = self.serializer.version
+
+
 class TestHDF5Serializer:
     @pytest.mark.parametrize('dtype', ['list', 'recarray'])
-    def test_serialize(self, dtype):
+    def test_serialize(self, dtype, single_pair):
         filename = 'out.h5'
         classifier = DummyClassifier()
 
-        data = [(0, 1, 'A0', 'A1')]
         if dtype == 'list':
-            pairs = data
+            pairs = single_pair
         else:
-            pairs = np.rec.fromrecords(data, dtype=dtypes.pairs)
+            pairs = np.rec.fromrecords(single_pair, dtype=dtypes.pairs)
 
         s = HDF5Serializer(classifier, pairs)
         s.serialize(filename)
 
         with tables.open_file(filename) as hfile:
-            cgroup = hfile.root.classifier_info
-            assert cgroup.classname.read() == 'classiflib.test.test_serializers.DummyClassifier'.encode()
+            group = hfile.root.classifier.info
+            assert group.classname.read() == 'classiflib.test.test_serializers.DummyClassifier'.encode()
