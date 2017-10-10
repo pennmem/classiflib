1b754c1d5c4512a420dddefadd33fc0b00a13408
Michael V. DePalatis <mike@depalatis.net>
2017-10-10 11:26:55 -0400

    Release version 0.1.0

--- classiflib/__init__.py
+++ classiflib/__init__.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-__version__ = '0.1.dev0'
+__version__ = '0.1.0'
 version_info = namedtuple("VersionInfo", "major,minor,patch")(*__version__.split('.'))
 
 from .defaults import FRDefaults
