6b5716de980206a09621957e49c757bd70619fad
Michael V. DePalatis <mike@depalatis.net>
2017-10-27 15:57:13 -0400

    Handle arbitrary exceptions in git_revision

--- classiflib/util.py
+++ classiflib/util.py
@@ -1,7 +1,13 @@
 from subprocess import Popen, PIPE
 import json
+import logging
+
 import numpy as np
+
 from classiflib import dtypes
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 def git_revision():
@@ -9,17 +15,22 @@ def git_revision():
     elsewhere, return 'unknown'.
 
     """
-    p = Popen('git ls-files'.split(), stdout=PIPE, stderr=PIPE)
-    stdout, _ = p.communicate()
-    if not len(stdout):  # pragma: nocover
-        # we're not in a git repository
-        return 'unknown'
+    try:
+        p = Popen('git ls-files'.split(), stdout=PIPE, stderr=PIPE)
+        stdout, _ = p.communicate()
+        if not len(stdout):  # pragma: nocover
+            # we're not in a git repository
+            return 'unknown'
 
-    p = Popen('git rev-parse HEAD'.split(), stdout=PIPE, stderr=PIPE)
-    stdout, _ = p.communicate()
-    if len(stdout) > 0:
-        return stdout.decode().strip()
-    else:  # pragma: nocover
+        p = Popen('git rev-parse HEAD'.split(), stdout=PIPE, stderr=PIPE)
+        stdout, _ = p.communicate()
+        if len(stdout) > 0:
+            return stdout.decode().strip()
+        else:  # pragma: nocover
+            return 'unknown'
+    except Exception as e:
+        # Handle anything bad that happens (such as not having git installed)
+        logger.error("Error running git_revision", exc_info=True)
         return 'unknown'
 
 
