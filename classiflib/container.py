0ca22e7949baa536e917bfbb15d71de8256b41f3
Michael V. DePalatis <mike@depalatis.net>
2018-04-03 17:09:17 -0400

    Add __str__ and __repr__ methods
    Closes #13

--- classiflib/container.py
+++ classiflib/container.py
@@ -231,6 +231,14 @@ class OdinEmbeddedClassifierContainer(object):
             num_classifiers=len(self.classifiers)
         )
 
+    def __str__(self):
+        return "<OdinEmbeddedClassifierContainer({}, {} classifiers, {} channels)>".format(
+            self.subject, len(self.classifiers), len(self.channels)
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
     def __eq__(self, other):
         if not self.classifiers == other.classifiers:
             return False
